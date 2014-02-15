54e5810ff9cff381582765340a74045596aa8d6b
Runar Flåten <runarflaaten@gmail.com>
2014-02-15 14:08:42 +0100

    Refractor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -22,8 +22,8 @@ class NewVisitorTest (unittest.TestCase):
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
-		# She is invited to enter a to do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		# She is invited to enter a to do item straight inputbox
+		away = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# She types "Buy peacock feathers" into a text box
