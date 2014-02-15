8b36e9dc90217f57d54cace240db286c182c4661
Runar Flåten <runarflaaten@gmail.com>
2014-02-15 16:43:26 +0100

    Move functional_tests to functional_tests app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+from django.test import LiveServerTestCase
 
-class NewVisitorTest (unittest.TestCase):
+class NewVisitorTest (LiveServerTestCase):
 
 	def setUp(self):
 
@@ -23,7 +24,7 @@ class NewVisitorTest (unittest.TestCase):
 
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepag
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to do list
 		self.assertIn('To-Do', self.browser.title)
@@ -52,7 +53,7 @@ class NewVisitorTest (unittest.TestCase):
 
 		# The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
 
